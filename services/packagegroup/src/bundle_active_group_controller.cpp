789f08805f3153cbb55ac221ced7aef36dd5b949
houdisheng <houdisheng@huawei.com>
2022-03-12 10:17:44 +0800

    增加日志打印
    Signed-off-by: houdisheng <houdisheng@huawei.com>

--- services/packagegroup/src/bundle_active_group_controller.cpp
+++ services/packagegroup/src/bundle_active_group_controller.cpp
@@ -54,6 +54,7 @@ void BundleActiveGroupController::OnUserSwitched(const int userId, const int las
 {
     BUNDLE_ACTIVE_LOGI("last time check for user %{public}d", lastUsedUser);
     CheckEachBundleState(lastUsedUser);
+    bundleUserHistory_->WriteBundleUsage(lastUsedUser);
     std::lock_guard<std::mutex> lock(mutex_);
     if (!activeGroupHandler_.expired()) {
         activeGroupHandler_.lock()->RemoveEvent(BundleActiveGroupHandler::MSG_CHECK_IDLE_STATE);
