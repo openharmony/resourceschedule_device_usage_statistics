b153a4be924c06ce22b70d3aef00d6859f79527a
houdisheng <houdisheng@huawei.com>
2022-04-15 16:07:34 +0800

    同步API评审意见
    Signed-off-by: houdisheng <houdisheng@huawei.com>

--- interfaces/kits/bundlestats/js/@ohos.bundleState.d.ts
+++ interfaces/kits/bundlestats/js/@ohos.bundleState.d.ts
@@ -338,7 +338,7 @@ declare namespace bundleState {
      * @syscap SystemCapability.ResourceSchedule.UsageStatistics.App
      * @permission ohos.permission.BUNDLE_ACTIVE_INFO
      * @systemapi Hide this for inner system use.
-     * @param maxNum Indicates max record number in result, max is 1000.
+     * @param maxNum Indicates max record number in result, max value is 1000, default value is 1000.
      * @return Returns the {@link BundleActiveModuleInfo} object Array containing the state data of the current module.
      */
     function getRecentlyUsedModules(maxNum?: number, callback: AsyncCallback<Array<BundleActiveModuleInfo>>): void;
