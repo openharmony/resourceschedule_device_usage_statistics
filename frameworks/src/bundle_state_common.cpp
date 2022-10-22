8deaa9e3cb4d6812ff96ecdfd9107aa11ae32395
yupeng74@huawei.com <yupeng74@huawei.com>
2022-10-22 18:16:52 +0800

    版本兼容bugfix
    Signed-off-by: yupeng74@huawei.com <yupeng74@huawei.com>

--- frameworks/src/bundle_state_common.cpp
+++ frameworks/src/bundle_state_common.cpp
@@ -116,10 +116,10 @@ void BundleStateCommon::GetBundleActiveEventForResult(
 
         napi_value eventId = nullptr;
         NAPI_CALL_RETURN_VOID(env, napi_create_int32(env, item.eventId_, &eventId));
-        
+
         napi_value eventOccurredTime = nullptr;
         NAPI_CALL_RETURN_VOID(env, napi_create_int64(env, item.timeStamp_, &eventOccurredTime));
-        
+
         if (isNewVersion) {
             NAPI_CALL_RETURN_VOID(env, napi_set_named_property(env, bundleActiveState, "eventId", eventId));
             NAPI_CALL_RETURN_VOID(env, napi_set_named_property(env, bundleActiveState, "eventOccurredTime",
@@ -366,8 +366,8 @@ napi_value BundleStateCommon::GetErrorValue(napi_env env, int32_t errCode)
     NAPI_CALL(env, napi_create_int32(env, reflectCode, &eCode));
     NAPI_CALL(env, napi_create_string_utf8(env, errMsg.c_str(), errMsg.length(), &eMsg));
     NAPI_CALL(env, napi_create_object(env, &result));
-    NAPI_CALL(env, napi_set_named_property(env, result, "errCode", eCode));
-    NAPI_CALL(env, napi_set_named_property(env, result, "errMessage", eMsg));
+    NAPI_CALL(env, napi_set_named_property(env, result, "code", eCode));
+    NAPI_CALL(env, napi_set_named_property(env, result, "message", eMsg));
     return result;
 }
 
