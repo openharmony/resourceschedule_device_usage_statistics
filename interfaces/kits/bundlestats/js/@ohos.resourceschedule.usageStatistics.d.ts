0965c75ad2a2eb9f28ab79919cb2f355daf9900e
houdisheng <houdisheng@huawei.com>
2023-04-23 16:27:37 +0800

    告警修复过
    Signed-off-by: houdisheng <houdisheng@huawei.com>

--- interfaces/kits/bundlestats/js/@ohos.resourceschedule.usageStatistics.d.ts
+++ interfaces/kits/bundlestats/js/@ohos.resourceschedule.usageStatistics.d.ts
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-import { AsyncCallback , Callback} from './@ohos.base';
+import { AsyncCallback, Callback } from './@ohos.base';
 
 /**
  * Provides methods for managing bundle usage statistics,
