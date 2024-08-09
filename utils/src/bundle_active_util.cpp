a59ca079df838d9824b09729d878701f6a066fd6
fengyang <fengyang44@huawei.com>
2024-08-09 15:04:45 +0800

    根据时区适配不同周期的落盘结算时间
    Signed-off-by: fengyang <fengyang44@huawei.com>

--- utils/src/bundle_active_util.cpp
+++ utils/src/bundle_active_util.cpp
@@ -22,6 +22,8 @@ const int32_t MILLISECOND_TO_MICROSECOND = 1000;
 const int32_t MILLISECOND_TO_SECOND = 1000;
 const int32_t SECOND_TO_MILLISECOND = 1000;
 const int64_t ONE_DAY_TIME = 24 * 60 * 60 *1000LL;
+const int64_t WEEK_OFFSET = 6LL;
+const int64_t DAYS_OF_WEEK = 7LL;
 const int32_t PERIOD_DAILY = 0;
 const int32_t PERIOD_WEEKLY = 1;
 const int32_t PERIOD_MONTHLY = 2;
@@ -38,35 +40,31 @@ int64_t BundleActiveUtil::GetFFRTDelayTime(const int64_t& delayTime)
 
 int64_t BundleActiveUtil::GetIntervalTypeStartTime(const int64_t& timeStamp, const int32_t& intervalType)
 {
-    int64_t startTime = 0;
     time_t time = timeStamp / MILLISECOND_TO_SECOND;
     std::tm* tm_time = std::localtime(&time);
     tm_time->tm_hour = 0;
     tm_time->tm_min = 0;
     tm_time->tm_sec = 0;
+    if (intervalType == PERIOD_WEEKLY) {
+        int64_t weekday = tm_time->tm_wday;
+        time_t startOfDay = mktime(tm_time) * SECOND_TO_MILLISECOND;
+        time_t weekDayTime = (weekday + WEEK_OFFSET) % DAYS_OF_WEEK * ONE_DAY_TIME;
+        return startOfDay - weekDayTime;
+    }
     switch (intervalType) {
-        case PERIOD_YEARLY:
-            tm_time->tm_mon = 0;
-            tm_time->tm_mday = 1;
-            startTime = mktime(tm_time) * SECOND_TO_MILLISECOND;
+        case PERIOD_DAILY:
             break;
         case PERIOD_MONTHLY:
             tm_time->tm_mday = 1;
-            startTime = mktime(tm_time) * SECOND_TO_MILLISECOND;
             break;
-        case PERIOD_WEEKLY:
-            int64_t weekday = tm_time->tm_wday;
-            time_t startOfDay = mktime(tm_time) * SECOND_TO_MILLISECOND;
-            time_t weekDayTime = (weekday -1) * ONE_DAY_TIME;
-            startTime = startOfDay - weekDayTime;
-            break;
-        case PERIOD_DAILY:
-            startTime = mktime(tm_time) * SECOND_TO_MILLISECOND;
+        case PERIOD_YEARLY:
+            tm_time->tm_mon = 0;
+            tm_time->tm_mday = 1;
             break;
         default:
             break;
     }
-    return startTime;
+    return mktime(tm_time) * SECOND_TO_MILLISECOND;
 }
 
 int64_t BundleActiveUtil::GetMonthStartTime(const int64_t& timeStamp)
